9233bb49a6e9e321085a0c134c68dd46f12e437d
9seconds <nineseconds@yandex.ru>
2018-03-20 09:54:54 +0300

    Add tests for csvdb records

---
+++ csvdb/record_test.go
@@ -0,0 +1,35 @@
+package csvdb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOK(t *testing.T) {
+	record, err := NewRecord("RU", "Nizhniy Novgorod", "93.94.95.1", "93.94.95.255")
+	assert.Nil(t, err)
+	assert.Equal(t, record.Country, "ru")
+	assert.Equal(t, record.City, "Nizhniy Novgorod")
+	assert.Equal(t, record.StartIP, "93.94.95.1")
+	assert.Equal(t, record.FinishIP, "93.94.95.255")
+
+	subnets, err := record.GetSubnets()
+	assert.Nil(t, err)
+	assert.Len(t, subnets, 8)
+}
+
+func TestUnknownCountry(t *testing.T) {
+	for _, country := range []string{"zz", "zZ", "Zz", "ZZ"} {
+		_, err := NewRecord(country, "Nizhniy Novgorod", "93.94.95.1", "93.94.95.255")
+		assert.NotNil(t, err)
+	}
+}
+
+func TestIncorrectIP(t *testing.T) {
+	_, err := NewRecord("ru", "Moscow", "x", "93.94.95.255")
+	assert.NotNil(t, err)
+
+	_, err = NewRecord("ru", "Moscow", "93.94.95.255", "t")
+	assert.NotNil(t, err)
+}
