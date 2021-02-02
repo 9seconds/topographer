b9c57b58cce2ce920f39b44823631483dc8cac1c
9seconds <nineseconds@yandex.ru>
2021-02-02 15:43:56 +0300

    Add ipstack provider

---
+++ providers/ipstack.go
@@ -0,0 +1,111 @@
+package providers
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+
+	"github.com/9seconds/topographer/topolib"
+)
+
+const NameIPStack = "ipstack"
+
+type ipstackResponse struct {
+	Error struct {
+		Code int    `json:"code"`
+		Type string `json:"type"`
+		Info string `json:"info"`
+	} `json:"error"`
+	City    string `json:"city"`
+	Country string `json:"country_code"`
+}
+
+type ipstackProvider struct {
+	client     topolib.HTTPClient
+	httpScheme string
+	authToken  string
+}
+
+func (i ipstackProvider) Name() string {
+	return NameIPStack
+}
+
+func (i ipstackProvider) Lookup(ctx context.Context, ip net.IP) (topolib.ProviderLookupResult, error) {
+	result := topolib.ProviderLookupResult{}
+	getQuery := url.Values{}
+
+	getQuery.Set("access_key", i.authToken)
+	getQuery.Set("output", "json")
+	getQuery.Set("fields", "country_code,city")
+	getQuery.Set("language", "en")
+	getQuery.Set("hostname", "0")
+	getQuery.Set("security", "0")
+
+	u := &url.URL{
+		Scheme:   i.httpScheme,
+		Host:     "api.ipstack.com",
+		Path:     ip.String(),
+		RawQuery: getQuery.Encode(),
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return result, fmt.Errorf("cannot build a request: %w", err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := i.client.Do(req)
+	if err != nil {
+		return result, fmt.Errorf("cannot send a request: %w", err)
+	}
+
+	defer func() {
+		io.Copy(ioutil.Discard, resp.Body)
+		resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return result, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	jsonResponse := ipstackResponse{}
+	jsonDecoder := json.NewDecoder(bufio.NewReader(resp.Body))
+
+	if err := jsonDecoder.Decode(&jsonResponse); err != nil {
+		return result, fmt.Errorf("cannot parse a response: %w", err)
+	}
+
+	if jsonResponse.Error.Code != 0 {
+		return result, fmt.Errorf(
+			"failed response: code=%d, type=%s, info=%s",
+			jsonResponse.Error.Code,
+			jsonResponse.Error.Type,
+			jsonResponse.Error.Info)
+	}
+
+	result.City = jsonResponse.City
+	result.CountryCode = jsonResponse.Country
+
+	return result, nil
+}
+
+func NewIPStack(client topolib.HTTPClient, authToken string, isSecure bool) topolib.Provider {
+	scheme := "http"
+
+	if isSecure {
+		scheme = "https"
+	}
+
+	return ipstackProvider{
+		client:     client,
+		authToken:  authToken,
+		httpScheme: scheme,
+	}
+}
---
+++ providers/ipstack_test.go
@@ -0,0 +1,126 @@
+package providers_test
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/9seconds/topographer/providers"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/suite"
+)
+
+type MockedIPStackTestSuite struct {
+	OnlineProviderTestSuite
+	HTTPMockMixin
+}
+
+func (suite *MockedIPStackTestSuite) SetupTest() {
+	suite.OnlineProviderTestSuite.SetupTest()
+
+	suite.prov = providers.NewIPStack(suite.http, "token", true)
+}
+
+func (suite *MockedIPStackTestSuite) TestName() {
+	suite.Equal(providers.NameIPStack, suite.prov.Name())
+}
+
+func (suite *MockedIPStackTestSuite) TestLookupClosedContext() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cancel()
+
+	_, err := suite.prov.Lookup(ctx, net.ParseIP("23.22.13.113"))
+
+	suite.Error(err)
+}
+
+func (suite *MockedIPStackTestSuite) TestLookupFailed() {
+	httpmock.RegisterResponder("GET",
+		"https://api.ipstack.com/23.22.13.113",
+		httpmock.NewStringResponder(http.StatusInternalServerError, ""))
+
+	_, err := suite.prov.Lookup(context.Background(),
+		net.ParseIP("23.22.13.113"))
+
+	suite.Error(err)
+}
+
+func (suite *MockedIPStackTestSuite) TestLookupBadJSON() {
+	httpmock.RegisterResponder("GET",
+		"https://api.ipstack.com/23.22.13.113",
+		httpmock.NewStringResponder(http.StatusOK, "{["))
+
+	_, err := suite.prov.Lookup(context.Background(),
+		net.ParseIP("23.22.13.113"))
+
+	suite.Error(err)
+}
+
+func (suite *MockedIPStackTestSuite) TestLookupError() {
+	httpmock.RegisterResponder("GET",
+		"https://api.ipstack.com/23.22.13.113",
+		httpmock.NewStringResponder(http.StatusOK, `{
+"success": false,
+"error": {"code": 105, "info": "", "type": "some_error"}
+        }`))
+
+	_, err := suite.prov.Lookup(context.Background(),
+		net.ParseIP("23.22.13.113"))
+
+	suite.Error(err)
+}
+
+func (suite *MockedIPStackTestSuite) TestLookupOK() {
+	httpmock.RegisterResponder("GET",
+		"https://api.ipstack.com/23.22.13.113?access_key=token",
+		httpmock.NewStringResponder(http.StatusOK, `{
+"country_code": "RU", "city": "Moscow"
+        }`))
+
+	result, err := suite.prov.Lookup(context.Background(),
+		net.ParseIP("23.22.13.113"))
+
+	suite.Equal("RU", strings.ToUpper(result.CountryCode))
+	suite.Equal("Moscow", result.City)
+	suite.NoError(err)
+}
+
+type IntegrationIPStackTestSuite struct {
+	OnlineProviderTestSuite
+}
+
+func (suite *IntegrationIPStackTestSuite) SetupTest() {
+	suite.OnlineProviderTestSuite.SetupTest()
+
+	suite.prov = providers.NewIPStack(suite.http, os.Getenv("IPSTACK_API_KEY"), false)
+}
+
+func (suite *IntegrationIPStackTestSuite) TestLookup() {
+	result, err := suite.prov.Lookup(context.Background(),
+		net.ParseIP("23.22.13.113"))
+
+	suite.Equal("US", strings.ToUpper(result.CountryCode))
+	suite.NoError(err)
+}
+
+func TestIPStack(t *testing.T) {
+	suite.Run(t, &MockedIPStackTestSuite{})
+}
+
+func TestIntegrationIPStack(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipped becaue of the short mode")
+		return
+	}
+
+	if os.Getenv("IPSTACK_API_KEY") == "" {
+		t.Skip("Skipped because there is no IPSTACK_API_KEY in environment")
+		return
+	}
+
+	suite.Run(t, &IntegrationIPStackTestSuite{})
+}
