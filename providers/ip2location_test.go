59336facef3f1f852dde9302746ed7ec6285a05b
9seconds <nineseconds@yandex.ru>
2021-02-09 10:57:48 +0300

    Add tests for ip2location

--- providers/ip2location_test.go
+++ providers/ip2location_test.go
@@ -1,13 +1,19 @@
 package providers_test
 
 import (
+	"archive/zip"
+	"bytes"
 	"context"
+	"io/ioutil"
 	"net"
+	"net/http"
 	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
 	"github.com/9seconds/topographer/providers"
+	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -19,13 +25,17 @@ type MockedIP2LocationTestSuite struct {
 	HTTPMockMixin
 }
 
+func (suite *MockedIP2LocationTestSuite) BaseDirectory() string {
+	return filepath.Join(suite.GetTestdataPath(), "ip2location")
+}
+
 func (suite *MockedIP2LocationTestSuite) SetupTest() {
 	suite.TmpDirTestSuite.SetupTest()
 	suite.OfflineProviderTestSuite.SetupTest()
 
 	prov, err := providers.NewIP2Location(suite.http,
 		time.Minute,
-		suite.tmpDir,
+		suite.BaseDirectory(),
 		"token",
 		"DBTEST")
 	if err != nil {
@@ -39,6 +49,110 @@ func (suite *MockedIP2LocationTestSuite) TearDownTest() {
 	suite.HTTPMockMixin.TearDownTest()
 	suite.OfflineProviderTestSuite.TearDownTest()
 	suite.TmpDirTestSuite.TearDownTest()
+}
+
+func (suite *MockedIP2LocationTestSuite) TestName() {
+	suite.Equal(providers.NameIP2Location, suite.prov.Name())
+}
+
+func (suite *MockedIP2LocationTestSuite) TestUpdateEvery() {
+	suite.Equal(time.Minute, suite.prov.UpdateEvery())
+}
+
+func (suite *MockedIP2LocationTestSuite) TestBaseDirectory() {
+	suite.Equal(suite.BaseDirectory(), suite.prov.BaseDirectory())
+}
+
+func (suite *MockedIP2LocationTestSuite) TestDownloadUrlNotFound() {
+	ctx := context.Background()
+
+	httpmock.RegisterResponder("GET",
+		"https://www.ip2location.com/download/",
+		httpmock.NewStringResponder(http.StatusNotFound, ""))
+
+	suite.Error(suite.prov.Download(ctx, suite.tmpDir))
+}
+
+func (suite *MockedIP2LocationTestSuite) TestDownloadFSReadOnly() {
+	ctx := context.Background()
+
+	httpmock.RegisterResponder("GET",
+		"https://www.ip2location.com/download/",
+		httpmock.NewStringResponder(http.StatusOK, "123"))
+
+	suite.NoError(os.Chmod(suite.tmpDir, 0555))
+	suite.Error(suite.prov.Download(ctx, suite.tmpDir))
+}
+
+func (suite *MockedIP2LocationTestSuite) TestDownloadNoBin() {
+	ctx := context.Background()
+
+	zipBuf := &bytes.Buffer{}
+	zipWriter := zip.NewWriter(zipBuf)
+
+	fp, _ := zipWriter.Create("README.txt")
+
+    fp.Write([]byte{1, 2, 3}) // nolint: errcheck
+
+	zipWriter.Close()
+
+	httpmock.RegisterResponder("GET",
+		"https://www.ip2location.com/download/",
+		httpmock.NewBytesResponder(http.StatusOK, zipBuf.Bytes()))
+
+	suite.Error(suite.prov.Download(ctx, suite.tmpDir))
+}
+
+func (suite *MockedIP2LocationTestSuite) TestDownloadOk() {
+	ctx := context.Background()
+
+	zipBuf := &bytes.Buffer{}
+	zipWriter := zip.NewWriter(zipBuf)
+
+	fp, _ := zipWriter.Create("README.txt")
+
+    fp.Write([]byte{1, 2, 3}) // nolint: errcheck
+
+	fp, _ = zipWriter.Create("db.bin")
+
+    fp.Write([]byte{4, 5, 6}) // nolint: errcheck
+
+	zipWriter.Close()
+
+	httpmock.RegisterResponder("GET",
+		"https://www.ip2location.com/download/",
+		httpmock.NewBytesResponder(http.StatusOK, zipBuf.Bytes()))
+
+	suite.NoError(suite.prov.Download(ctx, suite.tmpDir))
+
+	content, err := ioutil.ReadFile(filepath.Join(suite.tmpDir, "database.bin"))
+
+	suite.NoError(err)
+	suite.Equal([]byte{4, 5, 6}, content)
+}
+
+func (suite *MockedIP2LocationTestSuite) TestOpenNothing() {
+    suite.Error(suite.prov.Open(suite.tmpDir))
+}
+
+func (suite *MockedIP2LocationTestSuite) TestReopen() {
+    suite.NoError(suite.prov.Open(suite.BaseDirectory()))
+    suite.NoError(suite.prov.Open(suite.BaseDirectory()))
+
+    ctx := context.Background()
+    res, err := suite.prov.Lookup(ctx, net.ParseIP("80.80.80.80"))
+
+    suite.NoError(err)
+    suite.Equal("Amsterdam", res.City)
+    suite.Equal("NL", res.CountryCode)
+
+    suite.Error(suite.prov.Open(suite.tmpDir))
+
+    res, err = suite.prov.Lookup(ctx, net.ParseIP("80.80.80.80"))
+
+    suite.NoError(err)
+    suite.Equal("Amsterdam", res.City)
+    suite.Equal("NL", res.CountryCode)
 }
 
 type IntegrationIP2LocationTestSuite struct {
@@ -75,8 +189,8 @@ func (suite *IntegrationIP2LocationTestSuite) TestFull() {
 	res, err := suite.prov.Lookup(context.Background(), net.ParseIP("80.80.80.80"))
 
 	suite.NoError(err)
-    suite.Equal("Amsterdam", res.City)
-    suite.Equal("NL", res.CountryCode)
+	suite.Equal("Amsterdam", res.City)
+	suite.Equal("NL", res.CountryCode)
 }
 
 func TestIP2Location(t *testing.T) {
